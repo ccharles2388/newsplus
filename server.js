543a9498187586a4eefcc7989bea703faca3aa0a
Curtis Charles <ccharles2388@gmail.com>
2020-01-11 12:04:55 -0500

    Made Mongo Database Access To Heroku

--- server.js
+++ server.js
@@ -21,48 +21,48 @@ const app = express();
 // Use morgan logger for logging requests
 app.use(logger('dev'));
 // Parse request body as JSON
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
 app.use(express.static('public'));
 
 // Connect to the Mongo DB
-mongoose.connect('mongodb://localhost/unit18Populater', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/unit18Populater', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
 // Routes
 
 // A GET route for scraping the Weather website
-app.get('/scrape', async function(req, res) {
+app.get('/scrape', async function (req, res) {
   // First, we grab the body of the html with axios
   const response = await axios.get('https://hackernoon.com/tagged/ai');
   // Then, we load that into cheerio and save it to $ for a shorthand selector
   const $ = cheerio.load(response.data);
-// console.log(response.data);
+  // console.log(response.data);
   // Now, we grab every div with the class story card  item within an article tag, and do the following:
-  $('div.story-card').each(function(i, element) {
+  $('div.story-card').each(function (i, element) {
     // Save an empty result object
     const result = {};
 
     // Add the text and href of every link, and save them as properties of the result object
-   
+
     result.title = $(this)
-    .children('div.excerpt').children("div.title").children('a').text();
+      .children('div.excerpt').children("div.title").children('a').text();
 
     result.link = $(this)
-        .children('div').children("a").attr('href');
-        
-   
-console.log({result});
+      .children('div').children("a").attr('href');
+
+
+    console.log({ result });
     // Create a new Article using the `result` object built from scraping
     db.Article.create(result)
-        .then(function(dbArticle) {
-          // View the added result in the console
-          // console.log(dbArticle);
-        })
-        .catch(function(err) {
-          // If an error occurred, log it
-          // console.log(err);
-        });
+      .then(function (dbArticle) {
+        // View the added result in the console
+        // console.log(dbArticle);
+      })
+      .catch(function (err) {
+        // If an error occurred, log it
+        // console.log(err);
+      });
   });
 
   // Send a message to the client
@@ -70,44 +70,44 @@ console.log({result});
 });
 
 // Route for getting all Articles from the db
-app.get('/api/articles', async function(req, res) {
+app.get('/api/articles', async function (req, res) {
   // Grab every document in the Articles collection
   try {
     const data = await db.Article.find({});
     res.json(data);
   } catch (err) {
-    res.status(500).json({error: {name: err.name, message: err.message}});
+    res.status(500).json({ error: { name: err.name, message: err.message } });
   }
 });
 
 // Route for grabbing a specific Article by id, populate it with it's note
-app.get('/api/articles/:id', async function(req, res) {
+app.get('/api/articles/:id', async function (req, res) {
   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
   try {
-    const data = await db.Article.findOne({_id: req.params.id})
-        .populate('note'); // ..and populate all of the notes associated with it
+    const data = await db.Article.findOne({ _id: req.params.id })
+      .populate('note'); // ..and populate all of the notes associated with it
     res.json(data);
   } catch (err) {
-    res.status(500).json({error: {name: err.name, message: err.message}});
+    res.status(500).json({ error: { name: err.name, message: err.message } });
   }
 });
 
 // Route for saving/updating an Article's associated Note
-app.post('/api/articles/:id', async function(req, res) {
+app.post('/api/articles/:id', async function (req, res) {
   // Create a new note and pass the req.body to the entry
   try {
     const dbNote = await db.Note.create(req.body);
     // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`.
     // Update the Article to be associated with the new Note
     // { new: true } tells the query that we want it to return the updated Article -- it returns the original by default.
-    const dbArticle = await db.Article.findOneAndUpdate({_id: req.params.id}, {note: dbNote._id}, {new: true});
+    const dbArticle = await db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
     res.json(dbArticle);
   } catch (err) {
-    res.status(500).json({error: {name: err.name, message: err.message}});
+    res.status(500).json({ error: { name: err.name, message: err.message } });
   }
 });
 
 // Set the app to listen on PORT
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log('App running on http://localhost:%s', PORT);
 });
